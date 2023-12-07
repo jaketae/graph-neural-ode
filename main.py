6a6a642fb43b126b14630e6987c11a107662f8af
Jake Tae <jaesungtae@gmail.com>
2023-12-07 18:14:13 -0500

    chore: complete cleanup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import random
 import time
 
 import dgl
@@ -6,18 +8,16 @@ import dgl.data
 import lightning as L
 import matplotlib.pyplot as plt
 import torch
-from torch import nn
+from torch import nn, optim
 from torch.nn import functional as F
 
 from torchgde import GCNLayer, GDEFunc, ODEBlock, PerformanceContainer, accuracy
 from torchgde.models.odeblock import ODESolvers
 
-# import os
-# import random
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="test")
     parser.add_argument(
         "--dataset",
         default="cora",
@@ -47,6 +47,7 @@ def parse_args():
 
 
 def build_model(g, args, in_features, num_classes):
+    dropout = args.dropout
     hidden_channels = args.hidden_channels
     gnn = nn.Sequential(
         GCNLayer(
@@ -54,10 +55,14 @@ def build_model(g, args, in_features, num_classes):
             in_feats=hidden_channels,
             out_feats=hidden_channels,
             activation=nn.Softplus(),
-            dropout=0.9,
+            dropout=dropout,
         ),
         GCNLayer(
-            g=g, in_feats=hidden_channels, out_feats=hidden_channels, activation=None, dropout=0.9
+            g=g,
+            in_feats=hidden_channels,
+            out_feats=hidden_channels,
+            activation=None,
+            dropout=dropout,
         ),
     )
     gdefunc = GDEFunc(gnn)
@@ -106,150 +111,16 @@ def get_data(args):
     return dgl.data.PubmedGraphDataset()
 
 
-def main():
-    device = "cpu"
-    args = parse_args()
-    data = get_data(args)
-    L.seed_everything(args.seed)
-
-    # Cora is a node-classification datasets with 2708 nodes
-    X = torch.FloatTensor(data[0].ndata["feat"]).to(device)
-    Y = torch.LongTensor(data[0].ndata["label"]).to(device)
-
-    # In transductive semi-supervised node classification tasks on graphs, the model has access to all
-    # node features but only a masked subset of the labels
-    train_mask = torch.BoolTensor(data[0].ndata["train_mask"])
-    val_mask = torch.BoolTensor(data[0].ndata["val_mask"])
-    test_mask = torch.BoolTensor(data[0].ndata["test_mask"])
-
-    num_feats = X.shape[1]
-    n_classes = data.num_classes
-
-    # 140 training samples, 300 validation, 1000 test
-    n_classes, train_mask.sum().item(), val_mask.sum().item(), test_mask.sum().item()
-
-    # # add self-edge for each node
-    # g = data[0].graph
-    # g.remove_edges_from(nx.selfloop_edges(g))
-    # g.add_edges_from(zip(g.nodes(), g.nodes()))
-    # g = dgl.DGLGraph(g)
-    # edges = g.edges()
-    # n_edges = g.number_of_edges()
-
-    # n_edges
-    g = data[0]
-
-    # compute diagonal of normalization matrix D according to standard formula
-    degs = g.in_degrees().float()
-    norm = torch.pow(degs, -0.5)
-    norm[torch.isinf(norm)] = 0
-    # add to dgl.Graph in order for the norm to be accessible at training time
-    g.ndata["norm"] = norm.unsqueeze(1)  # .to(device)
-
-    g = g.to(device)
-    """# Graph Neural Differential Equations (GDEs)
-
-    As Neural ODEs, GDEs require specification of an ODE function (`ODEFunc`), representing the set of layers that will be called repeatedly by the ODE solver, as well as an ODE block (`ODEBlock`), tasked with calling the ODE solver on the ODE function. The ODEFunc is passed to the ODEBlock at initialization.
-
-    We introduce the convolutional variant of GDEs, `GCDEs`. The only difference resides in the type of GNN layer utilized in the ODEFunc.
-
-    For adaptive step GDEs (dopri5) we increase the hidden dimension to 64 to reduce the stiffness of the ODE and therefore the number of ODEFunc evaluations (`NFE`: Number Function Evaluation)
-    """
-
-    # for GCDEs, the ODEFunc is specified by two GCN layers. Softplus is used as activation. Smoother activations
-    # have been observed to help avoid numerical instability and reduce stiffness of the ODE described
-    # by a repeated call to the ODEFunc. High dropout improves performance on transductive node classification
-    # tasks due to their small training sets. GDEs can take advantage of this property due to their 'deeper'
-    # computational graph. NOTE: too much dropout increases stiffness and therefore NFEs
-
-    m = build_model(g, args, num_feats, n_classes)
-    """### Training loop
-
-    We use standard hyperparameters for GCNs, namely `1e-2` learning rate and `5e-4` weight decay.
-    For a fair comparison, GDE-dpr5 should be evaluated against deeper GCN models (GCNs with 4+
-    layers). This is because datasets such as Cora penalize deeper models due to small training
-    sets and thus need for very strong regularizers. GDE-rk4, whose ODEFunc is evaluated only 4
-    times, should be compared with shallower GCN models.
-    """
-
-    opt = torch.optim.Adam(m.parameters(), lr=1e-3, weight_decay=5e-4)
-    criterion = torch.nn.CrossEntropyLoss()
-    logger = PerformanceContainer(
-        data={
-            "train_loss": [],
-            "train_accuracy": [],
-            "test_loss": [],
-            "test_accuracy": [],
-            "forward_time": [],
-            "backward_time": [],
-            "nfe": [],
-        }
-    )
-    steps = 3000
-    verbose_step = 1
-    num_grad_steps = 0
-
-    for i in range(steps):  # looping over epochs
-        m.train()
-        start_time = time.time()
-
-        outputs = m(X)
-        f_time = time.time() - start_time
-
-        nfe = m.ode.odefunc.nfe
-
-        y_pred = outputs
-
-        loss = criterion(y_pred[train_mask], Y[train_mask])
-        opt.zero_grad()
-
-        start_time = time.time()
-        loss.backward()
-        b_time = time.time() - start_time
-
-        opt.step()
-        num_grad_steps += 1
-
-        with torch.inference_mode():
-            m.eval()
-
-            # calculating outputs again with zeroed dropout
-            y_pred = m(X)
-            m.ode.odefunc.nfe = 0
-
-            train_loss = loss.item()
-            train_acc = accuracy(y_pred[train_mask], Y[train_mask]).item()
-            test_acc = accuracy(y_pred[test_mask], Y[test_mask]).item()
-            test_loss = criterion(y_pred[test_mask], Y[test_mask]).item()
-            logger.deep_update(
-                logger.data,
-                dict(
-                    train_loss=[train_loss],
-                    train_accuracy=[train_acc],
-                    test_loss=[test_loss],
-                    test_accuracy=[test_acc],
-                    nfe=[nfe],
-                    forward_time=[f_time],
-                    backward_time=[b_time],
-                ),
-            )
-
-        if num_grad_steps % verbose_step == 0:
-            print(
-                "[{}], Loss: {:3.3f}, Train Accuracy: {:3.3f}, Test Accuracy: {:3.3f}, NFE: {}".format(
-                    num_grad_steps, train_loss, train_acc, test_acc, nfe
-                )
-            )
-
+def plot(logger, path):
     plt.figure(figsize=(16, 8))
     plt.subplot(2, 2, 1)
     plt.plot(logger.data["train_loss"])
-    plt.plot(logger.data["test_loss"])
-    plt.legend(["Train loss", "Test loss"])
+    plt.plot(logger.data["val_loss"])
+    plt.legend(["Train loss", "Validation loss"])
     plt.subplot(2, 2, 2)
     plt.plot(logger.data["train_accuracy"])
-    plt.plot(logger.data["test_accuracy"])
-    plt.legend(["Train accuracy", "Test accuracy"])
+    plt.plot(logger.data["val_accuracy"])
+    plt.legend(["Train accuracy", "Validation accuracy"])
     plt.subplot(2, 2, 3)
     plt.plot(logger.data["forward_time"])
     plt.plot(logger.data["backward_time"])
@@ -257,3 +128,106 @@ def main():
     plt.subplot(2, 2, 4)
     plt.plot(logger.data["nfe"], marker="o", linewidth=0.1, markersize=1)
     plt.legend(["NFE"])
+    plt.savefig(path)
+
+
+def main(args):
+    L.seed_everything(args.seed)
+    data = get_data(args)
+    device = torch.device("cuda")
+
+    X = data[0].ndata["feat"].to(device)
+    Y = data[0].ndata["label"].to(device)
+    train_mask = torch.BoolTensor(data[0].ndata["train_mask"])
+    val_mask = torch.BoolTensor(data[0].ndata["val_mask"])
+    test_mask = torch.BoolTensor(data[0].ndata["test_mask"])
+    num_feats = X.shape[1]
+    n_classes = data.num_classes
+    g = data[0]
+    g = dgl.add_self_loop(g)
+    degs = g.in_degrees().float()
+    norm = torch.pow(degs, -0.5)
+    norm[torch.isinf(norm)] = 0
+    g.ndata["norm"] = norm.unsqueeze(1)
+    g = g.to(device)
+
+    model = build_model(g, args, num_feats, n_classes).to(device)
+    print(f"parameters: {count_parameters(model)}")
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=5e-4)
+    logger = PerformanceContainer(
+        data={
+            "train_loss": [],
+            "train_accuracy": [],
+            "val_loss": [],
+            "val_accuracy": [],
+            "forward_time": [],
+            "backward_time": [],
+            "nfe": [],
+        }
+    )
+
+    best_val = 0
+    checkpoint_path = os.path.join("output", "checkpoints", f"{args.name}.pt")
+    for i in range(args.steps):
+        model.train()
+        optimizer.zero_grad()
+        if not (args.guide or args.fast):
+            t = 1
+        else:
+            t = random.uniform(0, 1)
+        start_time = time.time()
+        outputs = model(X, t)
+        f_time = time.time() - start_time
+        nfe = model.ode.odefunc.nfe
+        y_pred = outputs
+        if args.fast:
+            t = 1
+        loss = t * F.cross_entropy(y_pred[train_mask], Y[train_mask])
+        start_time = time.time()
+        loss.backward()
+        b_time = time.time() - start_time
+        optimizer.step()
+        with torch.inference_mode():
+            model.eval()
+            y_pred = model(X)
+            model.ode.odefunc.nfe = 0
+            train_loss = loss.item()
+            train_acc = accuracy(y_pred[train_mask], Y[train_mask]).item()
+            val_acc = accuracy(y_pred[val_mask], Y[val_mask]).item()
+            val_loss = F.cross_entropy(y_pred[val_mask], Y[val_mask]).item()
+            logger.deep_update(
+                logger.data,
+                dict(
+                    train_loss=[train_loss],
+                    train_accuracy=[train_acc],
+                    val_loss=[val_loss],
+                    val_accuracy=[val_acc],
+                    nfe=[nfe],
+                    forward_time=[f_time],
+                    backward_time=[b_time],
+                ),
+            )
+            if i % args.verbose == 0:
+                print(
+                    "[{}], loss: {:3.3f}, train acc: {:3.3f}, val acc: {:3.3f}, nfe: {}".format(
+                        i, train_loss, train_acc, val_acc, nfe
+                    )
+                )
+            if val_acc > best_val:
+                best_val = val_acc
+                torch.save(model.state_dict(), checkpoint_path)
+
+    model.eval()
+    y_pred = model(X)
+    test_acc = accuracy(y_pred[test_mask], Y[test_mask]).item()
+    print(f"last test: {test_acc}")
+    model.load_state_dict(torch.load(checkpoint_path))
+    model.eval()
+    y_pred = model(X)
+    test_acc = accuracy(y_pred[test_mask], Y[test_mask]).item()
+    print(f"best test: {test_acc}")
+    plot(logger, checkpoint_path=os.path.join("output", "figs", f"{args.name}.png"))
+
+
+if __name__ == "__main__":
+    main(parse_args())
