45602d2c99cb3d0aa11107b1756f5a5cc5cdb406
Jake Tae <jaesungtae@gmail.com>
2023-12-07 15:50:03 -0500

    chore: setup precommit

--- torchgde/models/gcn.py
+++ torchgde/models/gcn.py
@@ -1,19 +1,26 @@
 import math
-import numpy as np
+from typing import Callable
+
 import dgl
 import dgl.function as fn
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
-
-from typing import Callable
+from torch.nn.parameter import Parameter
 
 
 class GCNLayer(nn.Module):
-    def __init__(self, g:dgl.DGLGraph, in_feats:int, out_feats:int, activation:Callable[[torch.Tensor], torch.Tensor],
-                 dropout:int, bias:bool=True):
+    def __init__(
+        self,
+        g: dgl.DGLGraph,
+        in_feats: int,
+        out_feats: int,
+        activation: Callable[[torch.Tensor], torch.Tensor],
+        dropout: int,
+        bias: bool = True,
+    ):
         super().__init__()
         self.g = g
         self.weight = nn.Parameter(torch.Tensor(in_feats, out_feats))
@@ -25,11 +32,11 @@ class GCNLayer(nn.Module):
         if dropout:
             self.dropout = nn.Dropout(p=dropout)
         else:
-            self.dropout = 0.
+            self.dropout = 0.0
         self.reset_parameters()
 
     def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
+        stdv = 1.0 / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
@@ -39,13 +46,12 @@ class GCNLayer(nn.Module):
             h = self.dropout(h)
         h = torch.mm(h, self.weight)
         # normalization by square root of src degree
-        h = h * self.g.ndata['norm']
-        self.g.ndata['h'] = h
-        self.g.update_all(fn.copy_src(src='h', out='m'),
-                          fn.sum(msg='m', out='h'))
-        h = self.g.ndata.pop('h')
+        h = h * self.g.ndata["norm"]
+        self.g.ndata["h"] = h
+        self.g.update_all(fn.copy_u("h", "m"), fn.sum(msg="m", out="h"))
+        h = self.g.ndata.pop("h")
         # normalization by square root of dst degree
-        h = h * self.g.ndata['norm']
+        h = h * self.g.ndata["norm"]
         # bias
         if self.bias is not None:
             h = h + self.bias
@@ -55,8 +61,17 @@ class GCNLayer(nn.Module):
 
 
 class GCN(nn.Module):
-    def __init__(self, num_layers:int, g:dgl.DGLGraph, in_feats:int, hidden_feats:int,
-                 out_feats:int, activation:Callable, dropout:int, bias=True):
+    def __init__(
+        self,
+        num_layers: int,
+        g: dgl.DGLGraph,
+        in_feats: int,
+        hidden_feats: int,
+        out_feats: int,
+        activation: Callable,
+        dropout: int,
+        bias=True,
+    ):
         super().__init__()
         self.layers = nn.ModuleList()
 
@@ -65,15 +80,14 @@ class GCN(nn.Module):
         for i in range(num_layers - 2):
             self.layers.append(GCNLayer(g, hidden_feats, hidden_feats, activation, dropout))
 
-        self.layers.append(GCNLayer(g, hidden_feats, out_feats, None, 0.))
+        self.layers.append(GCNLayer(g, hidden_feats, out_feats, None, 0.0))
 
     def set_graph(self, g):
-        for l in self.layers:
-            l.g = g
+        for layer in self.layers:
+            layer.g = g
 
     def forward(self, features):
         h = features
         for layer in self.layers:
             h = layer(h)
         return h
-
